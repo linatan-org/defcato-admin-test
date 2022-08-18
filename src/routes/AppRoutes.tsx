f1f96a7a740e42c7116a545ec12beb3953ec3452
ST <workinvs@gmail.com>
2022-08-18 15:57:58 +0300

    added auth

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,25 +1,32 @@
 import { Redirect, Switch, Route } from 'react-router-dom';
-import { SignIn } from '../components/pages/SignIn/SignIn';
+import { SignIn } from '../components/pages/SignIn';
 import { routes } from '../constants/routes';
 import useAuth from '../contexts/auth/hook';
 import { NotFound } from '../components/pages/NotFound';
+import { Dashboard } from '../components/pages/Dashboard';
+import { ToastContainer } from 'react-toastify';
+
 const AppRoutes = () => {
   const authContext = useAuth();
   return (
-    <Switch>
-      <Route
-        path="/"
-        exact
-        render={() =>
-          authContext.isAuthenticated() ? (
-            <Redirect to={routes.dashboard} />
-          ) : (
-            <Route path="/" component={SignIn} />
-          )
-        }
-      />
-      <Route path="*" component={NotFound} />
-    </Switch>
+    <>
+      <ToastContainer />
+      <Switch>
+        <Route
+          path="/"
+          exact
+          render={() =>
+            authContext.isAuthenticated() || sessionStorage.token ? (
+              <Redirect to={routes.dashboard} />
+            ) : (
+              <Route path="/" component={SignIn} />
+            )
+          }
+        />
+        <Route path={routes.dashboard} component={Dashboard} />
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </>
   );
 };
 
